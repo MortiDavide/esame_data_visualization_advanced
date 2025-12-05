0b1491bede5b1d57f1fe7bc9a3d0fb22a28bf832
Alessandro Tag <alessandro.tag@itsrizzoli.it>
2025-12-05 09:53:45 +0100

    Creazione feature Panoramica

--- proviamo.py
+++ proviamo.py
@@ -22,9 +22,26 @@ SALES_COLS = ["NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales"]
 def load_data() -> pd.DataFrame:
     df = pd.read_csv(DATA_PATH)
 
+    #Years_of_Release è una colonna float, quindi la converto in int
+    df["Year_of_Release"] = pd.to_numeric(df["Year_of_Release"], errors="coerce")
+
     return df
 
 
-# APP ---------------------------------
-
+# --------------------------------- APP ---------------------------------
 df = load_data()
+
+st.title("🎮 Videogames Sales – Analytics Dashboard")
+st.caption("Dashboard esplorativa del catalogo videogiochi con dati di vendita globali")
+
+# PANORAMICA ---------------------------------
+st.subheader("📊 Panoramica del Dataset")
+
+col1, col2, col3 = st.columns(3)
+
+col1.metric("Numero giochi", len(df))
+col2.metric("Numero piattaforme", df["Platform"].nunique())
+col3.metric("Numero generi", df["Genre"].nunique())
+
+with st.expander("Mostra prime righe del dataset"):
+    st.dataframe(df.head())
